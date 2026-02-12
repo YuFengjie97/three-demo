844fe1a8a4e3c8683f0a7250dbf5853d89ae4f01
yufengjie <2457522528@qq.com>
2026-02-12 21:20:38 +0800

    1

--- app/routes/three/modelVertexCol/index.tsx
+++ app/routes/three/modelVertexCol/index.tsx
@@ -5,14 +5,22 @@ import * as THREE from 'three'
 import CustomShaderMaterial from 'three-custom-shader-material'
 import vertex from './vertex.glsl'
 import fragment from './fragment.glsl'
+import { mergeGeometries } from 'three/examples/jsm/utils/BufferGeometryUtils.js'
 
 function Base() {
-  const { nodes, materials } = useGLTF(
-    asset('/model/plane-pic-vertexCol-transformed.glb'),
-  )
-  // @ts-ignore
-  const geo = nodes.plane.geometry
-  console.log(geo)
+  // const { nodes, materials, animations } = useGLTF('/model/guangzhuzi.glb')
+  const { nodes, materials, animations } = useGLTF('/model/guangzhuzi-transformed.glb')
+  
+
+  const geos: THREE.BufferGeometry[] = []
+  ;(nodes.Scene as THREE.Scene).traverse((obj) => {
+    if(obj instanceof THREE.Mesh && obj.geometry){
+      geos.push(obj.geometry)
+    }
+  })
+
+  const geo = mergeGeometries(geos)  
+
 
   const tex = useTexture(asset('/img/texture/particle/star_09.png'))
   const uniforms = {
@@ -20,17 +28,14 @@ function Base() {
   }
 
   return (
-    // <mesh geometry={geo} rotation-x={Math.PI/2.}>
-    //   <meshBasicMaterial vertexColors={true}/>
-    // </mesh>
-    <points geometry={geo} scale={2.} rotation-x={Math.PI/2.}>
+    <points geometry={geo} scale={.01} rotation-x={-Math.PI/2-.1}>
       <CustomShaderMaterial
         baseMaterial={THREE.PointsMaterial}
         uniforms={uniforms}
-        // vertexColors={true}
-        vertexShader={vertex}
-        fragmentShader={fragment}
-        size={.05}
+        vertexColors={true}
+        // vertexShader={vertex}
+        // fragmentShader={fragment}
+        size={.01}
         transparent={true}
         // alphaTest={.1}
         // depthWrite={false}

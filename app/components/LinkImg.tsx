5b991cd9f50dc32b93dcc58f7c28d1e1fa644442
yufengjie <2457522528@qq.com>
2026-02-28 10:56:51 +0800

    imglink ui

--- app/components/LinkImg.tsx
+++ app/components/LinkImg.tsx
@@ -1,16 +1,53 @@
+import { useEffect, useRef, useState } from 'react'
 import { NavLink } from 'react-router'
 import { asset } from '~/utils/asset'
 
 export default function main({ to, img }: { to: string; img: string }) {
   const imgUrl = asset(`${img}`)
+  const [isLoading, setIsLoading] = useState(true)
+
+  const imgRef = useRef<HTMLImageElement>(null!)
+  useEffect(() => {
+    // 如果图片存在，且浏览器报告它已经加载完毕 (.complete 为 true)
+    if (imgRef.current && imgRef.current.complete) {
+      console.log('图片来自缓存，已直接加载完成')
+      setIsLoading(false)
+    }
+  }, [])
 
   return (
-    <div className='inline-block w-2xs h-2xs'>
+    <div className='inline-block w-40 h-30 transition-all duration-300
+      m-2
+      rounded-md overflow-hidden
+      hover:shadow-lg hover:shadow-emerald-100
+      hover:translate-y-1
+    '>
       <NavLink
-        className='inline-block h-full overflow-hidden mr-4 mb-2 rounded-md'
+        className='inline-block relative w-full h-full  mr-4 mb-2'
         to={to}
       >
-        <img className='block w-full object-cover object-center' src={imgUrl} />
+        {isLoading && (
+          <div className='absolute inset-0 bg-white flex items-center justify-center text-gray-500 z-10'>
+            {/* 你也可以在这里放一个 SVG Loading 图标 */}
+            <span>预览图加载中...</span>
+          </div>
+        )}
+        <img
+          className={`
+            block w-full h-full object-cover object-center transition-opacity duration-300
+            ${isLoading ? 'opacity-0' : 'opacity-100'}
+            `}
+          ref={imgRef}
+          src={imgUrl}
+          onLoad={() => {
+            console.log('previewImg ok')
+            setIsLoading(false)
+          }}
+          onError={(e) => {
+            console.log('预览图加载失败Error', e)
+            setIsLoading(false)
+          }}
+        />
       </NavLink>
     </div>
   )
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,63 +1,68 @@
-import type { Route } from "./+types/home";
+import type { Route } from './+types/home'
 import Link from '~/components/Link'
 import LinkImg from '~/components/LinkImg'
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+    { title: 'New React Router App' },
+    { name: 'description', content: 'Welcome to React Router!' },
+  ]
 }
 
 export default function Home() {
   return (
     <>
-    <div className="py-20 px-10">
-      <Link to='/reactTest'>test</Link>
-      <Link to='/three/some-test'>test</Link>
+      <div className='py-20 px-10'>
+        {/* <div className='flex flex-wrap gap-4'>
+          <Link to='/reactTest'>test</Link>
+          <Link to='/three/some-test'>test</Link>
+        </div> */}
 
-      <h1 className="text-7xl mb-10 text-amber-50">THREE.js</h1>
-      {/* <Link to=''>custom shader material</Link> */}
-      <LinkImg to='/three/csm' img='/img/preview/1.png'/>
+        <h1 className='text-7xl mb-10 text-amber-50'>THREE.js</h1>
+        {/* <Link to=''>custom shader material</Link> */}
+        <div className='flex flex-wrap gap-4'>
+          <LinkImg to='/three/csm' img='/img/preview/1.png' />
 
-      <LinkImg to='/three/sea' img='/img/preview/2.png' />
-      <LinkImg to='/three/points' img='/img/preview/3.png' />
-      <LinkImg to='/three/planet' img='/img/preview/4.png' />
-      <LinkImg to='/three/real-earth' img='/img/preview/5.png'/>
-      <LinkImg to='/three/trail'img='/img/preview/6.png'/>
-      <LinkImg to='/three/cloth-pattern'img='/img/preview/7.png' />
-      <LinkImg to='/three/china-3d'img='/img/preview/8.png' />
-      <LinkImg to='/three/flow-point-line'img='/img/preview/9.png'/>
-      <LinkImg to='/three/hexagon-vertex-offset'img='/img/preview/10.png'/>
-      <LinkImg to='/three/boxframes'img='/img/preview/11.png'/>
-      <LinkImg to='/three/swords'img='/img/preview/12.png' />
-      <LinkImg to='/three/fulu'img='/img/preview/13.png'/>
-      <LinkImg to='/three/line'img='/img/preview/14.png'/>
-      <LinkImg to='/three/line2'img='/img/preview/15.png'/>
-      <LinkImg to='/three/tube-line'img='/img/preview/16.png'/>
-      <LinkImg to='/three/tube-line2'img='/img/preview/17.png'/>
-      <LinkImg to='/three/zuowangdao'img='/img/preview/18.png'/>
-      <LinkImg to='/three/particle1'img='/img/preview/19.png'/>
-      <LinkImg to='/three/particle2'img='/img/preview/20.png'/>
-      <LinkImg to='/three/particle4-universe'img='/img/preview/21.png'/>
-      <LinkImg to='/three/particle5-storm'img='/img/preview/29.png'/>
+          <LinkImg to='/three/sea' img='/img/preview/2.png' />
+          <LinkImg to='/three/points' img='/img/preview/3.png' />
+          <LinkImg to='/three/planet' img='/img/preview/4.png' />
+          <LinkImg to='/three/real-earth' img='/img/preview/5.png' />
+          <LinkImg to='/three/trail' img='/img/preview/6.png' />
+          <LinkImg to='/three/cloth-pattern' img='/img/preview/7.png' />
+          <LinkImg to='/three/china-3d' img='/img/preview/8.png' />
+          <LinkImg to='/three/flow-point-line' img='/img/preview/9.png' />
+          <LinkImg
+            to='/three/hexagon-vertex-offset'
+            img='/img/preview/10.png'
+          />
+          <LinkImg to='/three/boxframes' img='/img/preview/11.png' />
+          <LinkImg to='/three/swords' img='/img/preview/12.png' />
+          <LinkImg to='/three/fulu' img='/img/preview/13.png' />
+          <LinkImg to='/three/line' img='/img/preview/14.png' />
+          <LinkImg to='/three/line2' img='/img/preview/15.png' />
+          <LinkImg to='/three/tube-line' img='/img/preview/16.png' />
+          <LinkImg to='/three/tube-line2' img='/img/preview/17.png' />
+          <LinkImg to='/three/zuowangdao' img='/img/preview/18.png' />
+          <LinkImg to='/three/particle1' img='/img/preview/19.png' />
+          <LinkImg to='/three/particle2' img='/img/preview/20.png' />
+          <LinkImg to='/three/particle4-universe' img='/img/preview/21.png' />
+          <LinkImg to='/three/particle5-storm' img='/img/preview/29.png' />
 
+          <LinkImg to='/three/ball' img='/img/preview/22.png' />
+          <LinkImg to='/three/tree' img='/img/preview/23.png' />
+          <LinkImg to='/three/growTree' img='/img/preview/24.png' />
+          <LinkImg to='/three/model-vertex-col' img='/img/preview/25.png' />
 
-      <LinkImg to='/three/ball'img='/img/preview/22.png'/>
-      <LinkImg to='/three/tree'img='/img/preview/23.png'/>
-      <LinkImg to='/three/growTree'img='/img/preview/24.png'/>
-      <LinkImg to='/three/model-vertex-col'img='/img/preview/25.png'/>
-      
-      <LinkImg to='/three/curve'img='/img/preview/27.png'/>
-      <LinkImg to='/three/curve-tube'img='/img/preview/28.png'/>
-      <LinkImg to='/three/model-line'img='/img/preview/0.png'/>
+          <LinkImg to='/three/curve' img='/img/preview/27.png' />
+          <LinkImg to='/three/curve-tube' img='/img/preview/28.png' />
+          {/* <LinkImg to='/three/model-line' img='/img/preview/0.png' /> */}
+        </div>
 
-
-      <h1 className="text-5xl my-10 text-amber-50">Music Visual</h1>
-      <LinkImg to='/three/mv1'img='/img/preview/26.png'/>
-
-
-    </div>
+        <h1 className='text-5xl my-10 text-amber-50'>Music Visual</h1>
+        <div className='flex flex-wrap gap-4'>
+          <LinkImg to='/three/mv1' img='/img/preview/26.png' />
+        </div>
+      </div>
     </>
   )
 }

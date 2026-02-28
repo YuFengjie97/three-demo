c0c59ce66dc067823b1a5a5fd0f2e1a1b6cdf8a9
yufengjie <2457522528@qq.com>
2026-02-28 16:06:14 +0800

    预览图占位骨架屏样式

--- app/components/LinkImg.tsx
+++ app/components/LinkImg.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useRef, useState } from 'react'
 import { NavLink } from 'react-router'
 import { asset } from '~/utils/asset'
+import styles from './LinkImg.module.css'
 
 export default function main({ to, img }: { to: string; img: string }) {
   const imgUrl = asset(`${img}`)
@@ -8,7 +9,7 @@ export default function main({ to, img }: { to: string; img: string }) {
 
   const imgRef = useRef<HTMLImageElement>(null!)
   useEffect(() => {
-    // 如果图片存在，且浏览器报告它已经加载完毕 (.complete 为 true)
+    // 如果图片存在(被浏览器缓存了)，且浏览器报告它已经加载完毕 (.complete 为 true)
     if (imgRef.current && imgRef.current.complete) {
       console.log('图片来自缓存，已直接加载完成')
       setIsLoading(false)
@@ -28,9 +29,9 @@ export default function main({ to, img }: { to: string; img: string }) {
         target='_blank'
       >
         {isLoading && (
-          <div className='absolute inset-0 bg-white flex items-center justify-center text-gray-500 z-10'>
-            {/* 你也可以在这里放一个 SVG Loading 图标 */}
-            <span>预览图加载中...</span>
+          <div className={`absolute
+            ${styles['preview-null']}
+          `}>
           </div>
         )}
         <img

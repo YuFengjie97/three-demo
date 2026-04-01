9fce37fb79364c5e068b532f461a84d80b5fcea8
yufengjie <2457522528@qq.com>
2026-04-01 17:34:27 +0800

    particle逻辑整理

--- app/routes/webgpu/particle/index.tsx
+++ app/routes/webgpu/particle/index.tsx
@@ -39,7 +39,6 @@ import { asset } from '~/utils/asset'
 
 import { type ThreeToJSXElements } from '@react-three/fiber'
 import { useControls } from 'leva'
-import { ResourceTracker } from '~/utils/resourceTracker'
 
 declare module '@react-three/fiber' {
   interface ThreeElements extends ThreeToJSXElements<typeof THREE> {}
@@ -48,44 +47,80 @@ declare module '@react-three/fiber' {
 extend(THREE as any)
 
 function Base() {
-  const rt = useMemo(() => new ResourceTracker(), [])
-
-  const tex = useTexture(asset('/img/texture/particle/star_09.png')) as unknown as THREE.Texture<HTMLImageElement>
+  const tex = useTexture(
+    asset('/img/texture/particle/star_09.png'),
+  ) as unknown as THREE.Texture<HTMLImageElement>
   // const tex = useMemo(() => {
   //   const loader = new THREE.TextureLoader();
   //   const tex =  loader.load( asset('/img/texture/particle/star_09.png') );
   //   return tex
   // }, [])
-  
-  rt.track(tex)
 
-  const uniforms = useMemo(() => ({
-    uLifeSpeed: uniform(0.1),
-    uSpeed: uniform(5),
-    uCol: uniform(color(new THREE.Color(0xff0000))),
-    uVelNoiseScale: uniform(0.1),
-    uHueOffsetScale: uniform(0.5),
-    uTex: (tex)
-  }), [])
+  const uniforms = useMemo(
+    () => ({
+      uLifeSpeed: uniform(0.1),
+      uSpeed: uniform(5),
+      uCol: uniform(color(new THREE.Color(0xff0000))),
+      uVelNoiseScale: uniform(0.1),
+      uHueOffsetScale: uniform(0.5),
+      uTex: tex,
+    }),
+    [],
+  )
 
   useControls({
-    lifeSpeed: { value: 0.1, min: 0, max: 2, step: 0.1, onChange: (v) => { uniforms.uLifeSpeed.value = v } },
-    speed: { value: 5, min: 0, max: 15, step: 0.1, onChange: (v) => { uniforms.uSpeed.value = v } },
-    col: { value: '#ff0000', onChange: (v) => { uniforms.uCol.value.set(v) } },
-    velNoiseScale: { value: 0.1, min: 0, max: 0.5, step: 0.01, onChange: (v) => { uniforms.uVelNoiseScale.value = v } },
-    uHueOffsetScale: { value: 0.5, min: 0, max: 1, step: 0.01, onChange: (v) => { uniforms.uHueOffsetScale.value = v } }
+    lifeSpeed: {
+      value: 0.1,
+      min: 0,
+      max: 2,
+      step: 0.1,
+      onChange: (v) => {
+        uniforms.uLifeSpeed.value = v
+      },
+    },
+    speed: {
+      value: 5,
+      min: 0,
+      max: 15,
+      step: 0.1,
+      onChange: (v) => {
+        uniforms.uSpeed.value = v
+      },
+    },
+    col: {
+      value: '#ff0000',
+      onChange: (v) => {
+        uniforms.uCol.value.set(v)
+      },
+    },
+    velNoiseScale: {
+      value: 0.1,
+      min: 0,
+      max: 0.5,
+      step: 0.01,
+      onChange: (v) => {
+        uniforms.uVelNoiseScale.value = v
+      },
+    },
+    uHueOffsetScale: {
+      value: 0.5,
+      min: 0,
+      max: 1,
+      step: 0.01,
+      onChange: (v) => {
+        uniforms.uHueOffsetScale.value = v
+      },
+    },
   })
 
+  const count = 60000
 
-  const { mesh, computeInit, computeUpdate } = useMemo(() => {
-
-    const geo = new THREE.PlaneGeometry(0.4, 0.4, 1, 1)
-    rt.track(geo)
-    const count = 60000
-
+  const {positionAttr, getColor, computeInit, computeUpdate} = useMemo(() => {
     const positionBuffer = instancedArray(count, 'vec3')
     const velBuffer = instancedArray(count, 'vec3')
     const lifeBuffer = instancedArray(count, 'float')
+
+    const positionAttr = positionBuffer.toAttribute()
 
     const initPos = Fn(() => {
       const idx = instanceIndex
@@ -95,9 +130,18 @@ function Base() {
         hash(idx.add(time).add(33.43)),
       )
         .sub(0.5)
-        .mul(vec3(40,2,40))
+        .mul(vec3(40, 2, 40))
       return pos
     })
+
+
+    const computeInit = Fn(() => {
+      const idx = instanceIndex
+      const posInit = initPos()
+      positionBuffer.element(idx).assign(posInit)
+      velBuffer.element(idx).assign(vec3(0, 0, 0))
+      lifeBuffer.element(idx).assign(hash(idx.add(5)))
+    })().compute(count)
 
     const updataVel = Fn(([pos]) => {
       const p = pos.mul(uniforms.uVelNoiseScale)
@@ -107,14 +151,6 @@ function Base() {
       const vz = mx_noise_vec3(p.add(vec3(0, 0, 1).mul(time)))
       return vec3(vx, vy, vz).normalize()
     })
-
-    const computeInit = Fn(() => {
-      const idx = instanceIndex
-      const posInit = initPos()
-      positionBuffer.element(idx).assign(posInit)
-      velBuffer.element(idx).assign(vec3(0, 0, 0))
-      lifeBuffer.element(idx).assign(hash(idx.add(5)))
-    })().compute(count)
 
     const computeUpdate = Fn(() => {
       const idx = instanceIndex
@@ -145,7 +181,9 @@ function Base() {
       const life = lifeBuffer.element(idx)
 
       const d = texture(tex).r
-      const hueOffset = dot(pos, vec3(0.1, 0.2, 0.3)).mul(uniforms.uHueOffsetScale)
+      const hueOffset = dot(pos, vec3(0.1, 0.2, 0.3)).mul(
+        uniforms.uHueOffsetScale,
+      )
       const col = hue(color(uniforms.uCol), hueOffset).mul(d)
 
       const fadeByLife = sin(life.mul(PI))
@@ -154,46 +192,37 @@ function Base() {
       return vec4(col, alpha)
     })
 
-
-    const mat = new THREE.SpriteNodeMaterial()
-    // const mat = new THREE.NodeMaterial()
-    rt.track(mat)
-
-    const posAttr = positionBuffer.toAttribute()
-    mat.positionNode = posAttr
-
-    mat.transparent = true
-    mat.depthWrite = false
-    // mat.alphaTest = 0.1
-    mat.blending = THREE.AdditiveBlending
-    mat.colorNode = getColor()
-
-    const mesh = new THREE.InstancedMesh(geo, mat, count)
-    rt.track(mesh)
-    return { mesh, computeInit, computeUpdate }
-  }, [])
+    return {positionAttr, getColor, computeInit, computeUpdate}
+  }, [count])
 
   const renderer = useThree().gl as unknown as THREE.WebGPURenderer
-
   useEffect(() => {
     renderer.compute(computeInit)
-    return () => {
-      rt.dispose()
-    }
   }, [])
-
   useFrame(() => {
     renderer.compute(computeUpdate)
   })
 
-  return <primitive object={mesh} />
+  return (
+    <instancedMesh args={[undefined, undefined, count]}>
+      <planeGeometry args={[0.4, 0.4, 1, 1]} />
+      <spriteNodeMaterial
+        positionNode={positionAttr}
+        transparent={true}
+        depthWrite={false}
+        // mat.alphaTest = 0.1
+        blending={THREE.AdditiveBlending}
+        colorNode={getColor()}
+      />
+    </instancedMesh>
+  )
 }
 
 export default function () {
   return (
     <div className='h-screen'>
       <Canvas
-        camera={{position: [0,5,15]}}
+        camera={{ position: [0, 5, 15] }}
         gl={async (props) => {
           const renderer = new THREE.WebGPURenderer(props as any)
           await renderer.init()

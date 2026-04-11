8c61d1e8b567c3e4195643429e5a37756a38a5f7
yufengjie <2457522528@qq.com>
2026-04-11 21:26:19 +0800

    1

--- app/routes.ts
+++ app/routes.ts
@@ -54,7 +54,6 @@ export default [
 
 
   route('/webgpu/link-particle', 'routes/webgpu/linkParticle/index.tsx'),
-  route('/webgpu/cube', 'routes/webgpu/cube/index.tsx'),
   route('/webgpu/particle', 'routes/webgpu/particle/index.tsx'),
   route('/webgpu/bar', 'routes/webgpu/bar/index.tsx'),
   route('/webgpu/turbulence', 'routes/webgpu/turbulence/index.tsx'),

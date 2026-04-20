24d6f01f2bd10ee73629d8f655878030cdbe86f7
yufengjie <2457522528@qq.com>
2026-04-20 15:28:08 +0800

    1

--- app/routes/webgpu/fragment/index.tsx
+++ app/routes/webgpu/fragment/index.tsx
@@ -54,6 +54,8 @@ import {
   transformedNormalWorld,
   rotate,
   mx_rotate3d,
+  attribute,
+  cos,
 } from "three/tsl";
 import * as THREE from "three/webgpu";
 import WebGPUCanvas from "~/components/WebGpuCanvas";
@@ -68,92 +70,103 @@ import { Pane } from "tweakpane";
 import { SkyboxFairyForestDay } from "~/components/model/SkyboxFairyForestDay";
 import { MeshSurfaceSampler } from "three/examples/jsm/Addons.js";
 
-function Lightning({ surface }: { surface: THREE.Mesh }) {
-  const count = 2;
-  
-  const map = useTexture(asset("/img/texture/particle/spark_05.png"));
-
-  const mat = useMemo(() => {
-    const sampler = new MeshSurfaceSampler(surface).setWeightAttribute("uv").build();
-    const position = new THREE.Vector3(0, 0, 0);
-    const positionArr = new Float32Array(count * 3);
-    for (let i = 0; i < count; i++) {
-      sampler.sample(position);
-      const i3 = i * 3;
-      positionArr[i3 + 0] = position.x;
-      positionArr[i3 + 1] = position.y;
-      positionArr[i3 + 2] = position.z;
-    }
-
-    const posBuffer = instancedArray(positionArr, "vec3");
-    const positionNode = Fn(() => {
-      const pos = posBuffer.element(instanceIndex).toVar();
-      const n = mx_noise_vec3(
-        positionLocal.mul(.4).add(1.).add(float(instanceIndex).mul(11.34)),
-      ).mul(2);
-      const rot = rotate(pos, n)
-      return rot.mul(positionLocal).add(pos);
-    })();
-
-    const colorNode = Fn(() => {
-
-      const uv2 = uv()
-      const noise = mx_noise_vec3(uv2.mul(20.1)).xy.mul(.5).add(.5)
-      // uv2.assign(mix(uv2, noise, .0))
-
-      const tex = texture(map, uv2).r;
-      const col = vec3(uv2, 0);
-
-      return vec4(col, 1);
-    })();
-    return { positionNode, colorNode };
-  }, [surface, count]);
-  return (
-    <instancedMesh args={[undefined, undefined, count]}>
-      <planeGeometry args={[4, 4, 30, 30]} />
-      <meshBasicMaterial
-        side={THREE.DoubleSide}
-        positionNode={mat.positionNode}
-        colorNode={mat.colorNode}
-        transparent={true}
-        depthWrite={false}
-        blending={THREE.AdditiveBlending}
-      />
-    </instancedMesh>
-  );
-}
-
 function Model() {
   const { nodes, materials } = useGLTF(asset("/model/ball_suipian-transformed.glb"));
 
   // @ts-ignore
-  const geo = nodes.plane.geometry as THREE.BufferGeometry;
-  const {mesh, uf} = useMemo(() => {
-    const uf = {
-      colSeed: uniform(vec3(3, 2, 1)),
-    };
+  // const geoBase = nodes.plane.geometry as THREE.BufferGeometry;
+  // const geoBase = new THREE.BoxGeometry(5, 5, 5,4,4,4);
+  // const geoBase = new THREE.PlaneGeometry(5,5)
+  const geoBase = new THREE.TorusKnotGeometry(5, 1., 50, 6)
+  console.log(geoBase);
+
+  const positionAttr = geoBase.getAttribute("position");
+  const indexArr = geoBase.getIndex()!.array;
+  const faceCount = indexArr.length / 3;
+
+  const mat = useMemo(() => {
+    const posArr: number[] = [];
+    const displaceDirArr: number[] = [];
+    const seedArr: number[] = []
+    const blockCenterArr: number[] = []
+
+    // 遍历三角面,形成三角体
+    // 三角面的三个顶点
+    const v0 = new THREE.Vector3();
+    const v1 = new THREE.Vector3();
+    const v2 = new THREE.Vector3();
+    const v3 = new THREE.Vector3(); // 三角面法线偏移的点(三角体补充的那个点)
+    const nor = new THREE.Vector3(); // 三角面的平面法线 或 三角体每个点的平移方向
+
+    const plane = new THREE.Plane();
+    for (let i = 0; i < faceCount; i++) {
+      const i3 = i * 3;
+      v0.fromBufferAttribute(positionAttr, indexArr[i3 + 0]);
+      v1.fromBufferAttribute(positionAttr, indexArr[i3 + 1]);
+      v2.fromBufferAttribute(positionAttr, indexArr[i3 + 2]);
+      plane.setFromCoplanarPoints(v0, v1, v2);
+      nor.copy(plane.normal);
+
+      v3.copy(v0).add(v1).add(v2).divideScalar(3); // 三角面中心
+      v3.add(nor.clone().multiplyScalar(-.2)); // 三角面中心点向平面法线方向偏移
+
+      // 三角体的4个面
+      posArr.push(...v0.toArray(),...v1.toArray(),...v2.toArray()); //012
+      posArr.push(...v3.toArray(),...v1.toArray(),...v0.toArray()); //312
+      posArr.push(...v3.toArray(),...v2.toArray(),...v1.toArray()); //321
+      posArr.push(...v3.toArray(),...v0.toArray(),...v2.toArray()); //302
+
+      // 为三角体的4个面指定相同位移方向属性
+      displaceDirArr.push(...nor.toArray(),...nor.toArray(),...nor.toArray())
+      displaceDirArr.push(...nor.toArray(),...nor.toArray(),...nor.toArray())
+      displaceDirArr.push(...nor.toArray(),...nor.toArray(),...nor.toArray())
+      displaceDirArr.push(...nor.toArray(),...nor.toArray(),...nor.toArray())
+
+      // 为三角体每个点设置seed
+      seedArr.push(i3,i3,i3)
+      seedArr.push(i3,i3,i3)
+      seedArr.push(i3,i3,i3)
+      seedArr.push(i3,i3,i3)
+
+
+      // 为三角体每个点指定三角体的中心
+      blockCenterArr.push(...v3.toArray(),...v3.toArray(),...v3.toArray())
+      blockCenterArr.push(...v3.toArray(),...v3.toArray(),...v3.toArray())
+      blockCenterArr.push(...v3.toArray(),...v3.toArray(),...v3.toArray())
+      blockCenterArr.push(...v3.toArray(),...v3.toArray(),...v3.toArray())
+    }
+
+    const geo = new THREE.BufferGeometry();
+    console.log({ posArr, displaceDirArr });
+
+    geo.setAttribute("position", new THREE.Float32BufferAttribute(posArr, 3));
+    geo.setAttribute('displaceDir', new THREE.Float32BufferAttribute(displaceDirArr, 3))
+    geo.setAttribute('seed', new THREE.Float32BufferAttribute(seedArr, 1))
+    geo.setAttribute('blockCenter', new THREE.Float32BufferAttribute(blockCenterArr, 3))
 
     const colorNode = Fn(() => {
-      const pos = positionLocal.toVar();
-      const col = sin3(uf.colSeed.add(dot(pos, vec3(0.15)).mul(2)))
-        .mul(0.5)
-        .add(0.5);
-      // return col
       return vec3(1);
     })();
 
-    const mat = new THREE.MeshPhysicalNodeMaterial()
-    mat.colorNode = colorNode
-    mat.roughness = .1
-    mat.metalness = 1.
-    const mesh = new THREE.Mesh(geo, mat)
-    mesh.rotation.z = Math.PI / 2
-    return { mesh, uf };
+    const displaceDir = attribute<'vec3'>('displaceDir', 'vec3')
+    const seed = attribute<'float'>('seed', 'float')
+    const blockCenter = attribute<'float'>('blockCenter', 'float')
+    const positionNode = Fn(() => {
+      const offsetStrength = smoothstep(0.5, 1., mx_noise_float(displaceDir.mul(2.).add(time)).mul(.5).add(.5)).mul(3)
+
+      // const cen = vec3(sin(time.mul(4)),0,0).mul(10)
+      // const offsetStrength = float(.1).div(blockCenter.distance(cen).mul(5).add(1))
+
+      const offset = displaceDir.mul(offsetStrength)
+      return positionLocal.add(offset)
+    })()
+
+    return { geo, colorNode, positionNode };
   }, []);
 
   const pane = new Pane();
   useEffect(() => {
-    pane.addBinding(uf.colSeed, "value", { label: "colSeed", min: 0, max: 10 });
+    // pane.addBinding(uf.colSeed, "value", { label: "colSeed", min: 0, max: 10 });
 
     return () => {
       pane.dispose();
@@ -161,11 +174,14 @@ function Model() {
   }, []);
 
   return (
-    <>
-      <primitive object={mesh} />
-      <Lightning surface={mesh}/>
-    </>
-  ) 
+    <mesh geometry={mat.geo}>
+      <meshPhysicalNodeMaterial 
+        roughness={.1}
+        metalness={.9}
+        positionNode={mat.positionNode}
+        colorNode={mat.colorNode} />
+    </mesh>
+  );
 }
 
 function Base() {
